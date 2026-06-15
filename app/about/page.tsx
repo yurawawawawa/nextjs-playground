5575c4fd03db948a618eeaf566b7790c05ae75e6
Arga Yura Danendra <argayura@gmail.com>
2026-06-15 14:24:13 +0700

    add basic and simple navbar

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>This page is for learning Next.js routing.</p>
+    </main>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <main>
+      <h1>Contact</h1>
+      <p>This is a simple contact page.</p>
+    </main>
+  );
+}
