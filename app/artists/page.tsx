1ff078cd190dac7ec617af7ad160d5e057ae0c6f
Arga Yura Danendra <argayura@gmail.com>
2026-06-18 20:29:38 +0700

    reusing main navbar for artists list page

--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -1,9 +1,23 @@
+import Navbar from "../components/navbar";
+
 export default function ArtistsPage() {
   return (
-    <main className="min-h-screen bg-[#F5F1E8] p-10">
-      <h1 className="text-5xl font-bold">Artists Archive</h1>
+    <main className="min-h-screen bg-[#F5F1E8] text-black">
+      <Navbar />
 
-      <p className="mt-4 text-zinc-600">Discover Indonesian indie artists.</p>
+      <section className="max-w-6xl mx-auto px-6 pt-20">
+        <h1
+          className="
+            text-6xl
+            font-sugo
+            font-black
+          "
+        >
+          Artists Archive
+        </h1>
+
+        <p className="mt-4 text-zinc-600">Discover Indonesian indie artists.</p>
+      </section>
     </main>
   );
 }
